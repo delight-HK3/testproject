edf2ad82042dcb9200bd10cbaad2080251f1e79b
leebean <dabin310153@naver.com>
2023-12-27 16:21:10 +0900

    소셜로그인 컨트롤러 수정
    안되네 씨부럴

--- src/main/java/com/toypro/test/toypro/controller/AccountController.java
+++ src/main/java/com/toypro/test/toypro/controller/AccountController.java
@@ -6,18 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.toypro.test.toypro.config.Constant.SocialLoginType;
 import com.toypro.test.toypro.service.OAuthService;
 
-@Controller
+@RestController
+@RequestMapping("/app/accounts")
 public class AccountController {
     
     @Autowired
     OAuthService oAuthService;
     
-    @GetMapping("/auth/{socialLoginType}")
+    
+    @GetMapping("auth/{socialLoginType}")
     public void socialLoginRedirect(@PathVariable(name="socialLoginType") String SocialLoginPath) throws IOException {
+        System.out.println("google");
+        
         SocialLoginType socialLoginType = SocialLoginType.valueOf(SocialLoginPath.toUpperCase());
         oAuthService.request(socialLoginType);
     }
