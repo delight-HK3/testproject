87cb37f1f8fcf352287b29f9e751b5d9edd667b0
leebean <dabin310153@naver.com>
2024-01-02 11:24:53 +0900

    사용안하는 import 삭제

--- src/main/java/com/toypro/test/toypro/controller/AccountController.java
+++ src/main/java/com/toypro/test/toypro/controller/AccountController.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toypro.test.toypro.config.Constant.SocialLoginType;
-import com.toypro.test.toypro.model.GetSocialOAuthRes;
 import com.toypro.test.toypro.service.OAuthService;
 
 import lombok.RequiredArgsConstructor;
