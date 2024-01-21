5e4aea6ce1b15e357533d83b04f1523f38d54475
leedabean <dabin310153@naver.com>
2024-01-21 23:28:17 +0900

    Update SocialLoginTypeConverter.java

--- src/main/java/com/toypro/test/toypro/converter/SocialLoginTypeConverter.java
+++ src/main/java/com/toypro/test/toypro/converter/SocialLoginTypeConverter.java
@@ -6,7 +6,7 @@ import org.springframework.core.convert.converter.Converter;
 
 @Configuration
 public class SocialLoginTypeConverter implements Converter<String, UserType> {
-     @Override
+    @Override
     public UserType convert(String s) {
         return UserType.valueOf(s.toUpperCase());
     }
