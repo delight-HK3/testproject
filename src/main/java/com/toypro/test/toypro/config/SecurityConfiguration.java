2007f50653201be3685896f46e696078019bafc4
leedabean <dabin310153@naver.com>
2024-02-26 23:42:42 +0900

    세션 관련 security 설정

--- src/main/java/com/toypro/test/toypro/config/SecurityConfiguration.java
+++ src/main/java/com/toypro/test/toypro/config/SecurityConfiguration.java
@@ -25,11 +25,18 @@ public class SecurityConfiguration {
     
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(
+		
+        http.csrf(
             (csrfConfig) -> csrfConfig.disable()
         ).headers(
             (headerConfig) ->  headerConfig.frameOptions(
                 frameOptionsConfig -> frameOptionsConfig.disable())
+        ).sessionManagement( // 세션 설정
+            (sessisonConfig) -> sessisonConfig.invalidSessionUrl("/") // 세션이 없는 경우 기본페이지               
+                                              .maximumSessions(1) // 최대 접속자 수
+                                              .maxSessionsPreventsLogin(true) // 최대 허용세션인 경우 사용자 인증실패 처리
+                                              .expiredUrl("/") // 세션이 만료된 경우 이동 할 페이지
+            
         );
 
         return http.build();
