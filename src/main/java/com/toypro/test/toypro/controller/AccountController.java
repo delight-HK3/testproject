d81c1bfa34650fa7117a56a4379787c7d49c8265
leebean <dabin310153@naver.com>
2024-03-10 17:00:12 +0900

    Update AccountController.java

--- src/main/java/com/toypro/test/toypro/controller/AccountController.java
+++ src/main/java/com/toypro/test/toypro/controller/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView; 
 
 import com.toypro.test.toypro.dto.social.SocialUserResponse;
 import com.toypro.test.toypro.service.UserService;
